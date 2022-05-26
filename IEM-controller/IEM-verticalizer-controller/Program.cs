0d1bb5636e4c85b60ca71954c6e514f4827dd5dd
modernpacifist <modernpacifist@gmail.com>
2022-05-26 15:14:50 +0300

    disabled implicitusings

--- IEM-controller/IEM-verticalizer-controller/Program.cs
+++ IEM-controller/IEM-verticalizer-controller/Program.cs
@@ -1,11 +1,19 @@
-﻿namespace IEM_verticalizer_controller
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IEM_verticalizer_controller
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            TableAPI.Table myTable = new TableAPI.Table();
-            myTable.Connect();
+            // TableAPI.Table myTable = new TableAPI.Table();
+            // myTable.Connect();
         }
     }
 }
