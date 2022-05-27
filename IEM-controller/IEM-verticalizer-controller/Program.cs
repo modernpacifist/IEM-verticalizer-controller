044e1b229ce8808809124121347ff41dff371f5a
modernpacifist <modernpacifist@gmail.com>
2022-05-27 15:07:56 +0300

    updated SetDirection/SetSpeed functions

--- IEM-controller/IEM-verticalizer-controller/Program.cs
+++ IEM-controller/IEM-verticalizer-controller/Program.cs
@@ -7,8 +7,8 @@ namespace IEM_verticalizer_controller
     internal class Program
     {
         // Terminate execution of a program with exit code if connection fails
-        static void InitiateConnection(ref Table table) {
-            if (!table.Connect()) {
+        static void InitiateConnection(ref Table tableInstance) {
+            if (!tableInstance.Connect()) {
                 Console.WriteLine("Connection failed, check physical state");
                 Environment.Exit(1);
             }
@@ -16,9 +16,48 @@ namespace IEM_verticalizer_controller
             return;
         }
 
+        static void SetDirection(ref Table tableInstance, bool direction) {
+            bool directionFlag = tableInstance.SetDirection(direction);
+            if (!directionFlag) {
+                Console.WriteLine("Speed was not set");
+                Environment.Exit(1);
+            }
+            if (direction) {
+                Console.WriteLine("Speed was set clockwise");
+            } else {
+                Console.WriteLine("Speed was set counter-clockwise");
+            }
+            return;
+        }
+
+        static void SetSpeed(ref Table tableInstance, float speedValue) {
+            if (0.05f > speedValue || speedValue > 0.5f) {
+                Console.WriteLine("Speed value must be in the interval of [0.05, 0.5]");
+                return;
+            }
+            bool speedFlag = tableInstance.SetSpeed(speedValue);
+            if (!speedFlag) {
+                Console.WriteLine("Speed was not set due to internal error");
+                return;
+            }
+            //Console.WriteLine("Speed was successfully set to: 0.5");
+            Console.WriteLine(String.Format("Speed was successfully set to: {0}", speedValue));
+            return;
+        }
+
         static void Main(string[] args) {
-            Table myTable = new Table();
-            InitiateConnection(ref myTable);
+            Table tableInstance = new Table();
+
+            //// Initiate connection with the table
+            //InitiateConnection(ref tableInstance);
+
+            // Set direction 
+
+            // Set speed of the rotation of the table
+            float sampleSpeed = 0.05f;
+            SetSpeed(ref tableInstance, sampleSpeed);
+
+
         }
     }
 }
