206aedd719b35e6d9ae1530a5ffede0bd772d938
modernpacifist <modernpacifist@gmail.com>
2022-06-06 11:20:01 +0300

    added beta version of a class TablePosition

--- IEM-controller/IEM-verticalizer-controller/Program.cs
+++ IEM-controller/IEM-verticalizer-controller/Program.cs
@@ -4,11 +4,51 @@ using TableAPI;
 
 namespace IEM_verticalizer_controller
 {
+    public class TablePosition
+    {
+        private double x = 0.0;
+        private double y = 0.0;
+
+        public void printPosition()
+        {
+            Console.WriteLine(x.ToString());
+            Console.WriteLine(y.ToString());
+        }
+
+        public void increaseX(double some_value)
+        {
+            this.x += some_value;
+        }
+
+        public void decreaseX(double some_value)
+        {
+            this.x += some_value;
+        }
+
+        public void increaseY(double some_value)
+        {
+            this.y += some_value;
+        }
+
+        public void decreaseY(double some_value)
+        {
+            this.y += some_value;
+        }
+    }
+
     internal class Program
     {
+        static void EmergencyStop(ref Table tableInstance)
+        {
+            // Intercept Ctrl+C from the user input during exeuciton and return table into horizontal position
+            return;
+        }
+
         // Terminate execution of a program with exit code if connection fails
-        static void InitiateConnection(ref Table tableInstance) {
-            if (!tableInstance.Connect()) {
+        static void InitiateConnection(ref Table tableInstance)
+        {
+            if (!tableInstance.Connect())
+            {
                 Console.WriteLine("Connection failed, check physical state");
                 Environment.Exit(1);
             }
@@ -16,27 +56,37 @@ namespace IEM_verticalizer_controller
             return;
         }
 
-        static void SetDirection(ref Table tableInstance, bool direction) {
+        static void SetDirection(ref Table tableInstance, bool direction)
+        {
             bool directionFlag = tableInstance.SetDirection(direction);
-            if (!directionFlag) {
+            if (!directionFlag)
+            {
                 Console.WriteLine("Direction was not set");
                 Environment.Exit(1);
             }
-            if (direction) {
+
+            if (direction)
+            {
                 Console.WriteLine("Direction set to CLOCKWISE");
-            } else {
+            } 
+            else
+            {
                 Console.WriteLine("Direction set to COUNTER-CLOCKWISE");
             }
             return;
         }
 
-        static void SetSpeed(ref Table tableInstance, float speedValue) {
-            if (0.05f > speedValue || speedValue > 0.5f) {
+        static void SetSpeed(ref Table tableInstance, float speedValue)
+        {
+            if (0.05f > speedValue || speedValue > 0.5f)
+            {
                 Console.WriteLine("Speed value must be in the interval of [0.05, 0.5]");
                 Environment.Exit(1);
             }
+
             bool speedFlag = tableInstance.SetSpeed(speedValue);
-            if (!speedFlag) {
+            if (!speedFlag)
+            {
                 Console.WriteLine("Speed was not set due to internal error");
                 return;
             }
@@ -44,9 +94,11 @@ namespace IEM_verticalizer_controller
             return;
         }
 
-        static void StartEngine(ref Table tableInstsance) {
+        static void StartEngine(ref Table tableInstsance)
+        {
             bool flag = tableInstsance.Start();
-            if (!flag) {
+            if (!flag)
+            {
                 Console.WriteLine("Engine start failed");
                 Environment.Exit(1);
             }
@@ -54,56 +106,65 @@ namespace IEM_verticalizer_controller
             return;
         }
 
-        static void StopEngine(ref Table tableInstsance) {
+        static void StopEngine(ref Table tableInstsance)
+        {
+            // add try/except block to stop engine definitely
             bool flag = tableInstsance.Stop();
-            if (!flag) {
+            if (!flag)
+            {
                 Console.WriteLine("ENGINE STOP FAILED");
             }
             Console.WriteLine("Engine is stopping...");
             return;
         }
 
-        static void ResetEngine(ref Table tableInstsance) {
+        static void ResetEngine(ref Table tableInstsance)
+        {
             bool flag = tableInstsance.Reset();
-            if (!flag) {
+            if (!flag)
+            {
                 Console.WriteLine("Engine was reset");
             }
             return;
         }
 
-        static void Main(string[] args) {
+        static void Main(string[] args)
+        {
             Table tableInstance = new Table();
             int timeInterval = 3000;
 
             // Initiate connection with the table
             InitiateConnection(ref tableInstance);
 
-            // Setting direction to clockwise
-            SetDirection(ref tableInstance, true);
+            TablePosition a = new TablePosition();
+            a.printPosition();
 
-            // Set speed of the rotation of the table
-            float sampleSpeed = 0.05f;
-            SetSpeed(ref tableInstance, sampleSpeed);
+            //// Set speed of the rotation of the table
+            //float sampleSpeed = 0.05f;
+            //SetSpeed(ref tableInstance, sampleSpeed);
 
-            // Start engine in clockwise direction
-            StartEngine(ref tableInstance);
-            System.Threading.Thread.Sleep(timeInterval);
+            //// Setting direction to clockwise
+            //SetDirection(ref tableInstance, true);
 
-            // Stopping engine
-            StopEngine(ref tableInstance);
+            //// Start engine clockwise
+            //StartEngine(ref tableInstance);
+            //System.Threading.Thread.Sleep(timeInterval);
 
-            // Setting direction to counter-clockwise
-            SetDirection(ref tableInstance, false);
+            //// Stopping engine
+            //StopEngine(ref tableInstance);
 
-            // Start engine in counter-clockwise direction
-            StartEngine(ref tableInstance);
-            System.Threading.Thread.Sleep(timeInterval);
+            //// Setting direction to counter-clockwise
+            //SetDirection(ref tableInstance, false);
 
-            // Stopping engine
-            StopEngine(ref tableInstance);
+            //// Start engine in counter-clockwise direction
+            //StartEngine(ref tableInstance);
+            //System.Threading.Thread.Sleep(timeInterval);
 
-            // Resetting engine
-            ResetEngine(ref tableInstance);
+            //// Stopping engine
+            //StopEngine(ref tableInstance);
+
+            //// Resetting engine
+            //ResetEngine(ref tableInstance);
         }
     }
 }
